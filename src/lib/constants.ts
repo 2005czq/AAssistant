ef7e82c45bd5ffe52a8eeba996e419c49b32fec0
2005czq <47026706+2005czq@users.noreply.github.com>
2026-09-24 11:30:09 +0800

    feat(constants): export demo url constant

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,5 @@
 export const GITHUB_URL = 'https://github.com/2005czq/AAssistant';
+export const DEMO_URL = 'https://2005czq.github.io/AAssistant';
 export const MAX_MEMBER_NAME_LENGTH = 10;
 export const MIN_MEMBERS = 2;
 export const MAX_MEMBERS = 15;
